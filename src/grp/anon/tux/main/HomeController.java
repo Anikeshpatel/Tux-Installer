a0cb17a2c81aaec9ca0a3bcf30e272721d1e46c3
Anikesh Patel <anikeshpatel4@gmail.com>
2018-02-09 12:10:22 +0530

    init Project

---
+++ src/grp/anon/tux/main/HomeController.java
@@ -0,0 +1,13 @@
+package grp.anon.tux.main;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
